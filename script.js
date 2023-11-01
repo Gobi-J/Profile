353587a0c493c8ef2e0f14bd9fc7ffba8a7779b4
Gobi J <2k20cse046@kiot.ac.in>
2023-11-01 16:15:28 +0530

    #

--- script.js
+++ script.js
@@ -21,14 +21,18 @@ toggleGreet.addEventListener('click', () => {
     toggleGreet.style.opacity = 0;
 });
 
-
+const userName = document.getElementById("greetingUser");
+if(localStorage.getItem("user")){
+    userName.innerHTML = `Welcome ${localStorage.getItem("user")}`;
+}
 function greet(){
     const name = document.querySelector("#pop input").value;
+    localStorage.setItem("user", name);
     
-    const userName = document.getElementById("greetingUser");
     userName.innerHTML = `Welcome ${name}`;
     popDiv.style.left = -250 + "px";
     toggleGreet.style.opacity = 1;
+    
     const contactName = document.getElementById("contactname")
     contactName.setAttribute("value", name)
 }
